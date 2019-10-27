6ec4d53bceacf0a752e1e40e024afddca19e6b9c
mhiemer <matthias.hiemer@exxcellent.de>
2019-10-27 08:28:17 +0100

    Added Event ressource and entity

---
+++ src/main/java/de/tpsw/domain/enumeration/EventType.java
@@ -0,0 +1,8 @@
+package de.tpsw.domain.enumeration;
+
+/**
+ * The EventType enumeration.
+ */
+public enum EventType {
+    ANIMAL_BORN, ANIMAL_DIED, AIR_CLEANED, AIR_POLLUTED, FOREST_SAVED, FOREST_DESTROYED, WATER_CLEANED, WATER_POLLUTED, DAILY_QUESTION
+}
